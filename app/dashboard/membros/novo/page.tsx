a6bc985d593c62f125cf0c65d066076d08261faa
Rene <rpgomes810@gmail.com>
2026-05-31 08:21:56 -0300

    Membros: instrumento Voz renomeado para Vocal

--- app/dashboard/membros/novo/page.tsx
+++ app/dashboard/membros/novo/page.tsx
@@ -139,7 +139,7 @@ export default function NovoMembro() {
                     <option value="Violino">Violino</option>
                     <option value="Viola">Viola</option>
                     <option value="Violoncelo">Violoncelo</option>
-                    <option value="Voz">Voz</option>
+                    <option value="Vocal">Vocal</option>
                   </select>
                 </div>
               </div>
